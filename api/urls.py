9b73ced0f91d62181eb8c2ac6f2d84b4a96ca93f
sadeeq <sadeejert@gmail.com>
2026-04-17 21:19:23 +0100

    fix trailing slash bug

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PersonPredictionView, PersonPredictionDetailView
 
 urlpatterns = [
-    path("profiles/", PersonPredictionView.as_view()),
+    path("profiles", PersonPredictionView.as_view()),
     path("profiles/<uuid:id>/", PersonPredictionDetailView.as_view(), name="person-detail"),
 ]
 
