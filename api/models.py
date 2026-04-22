047ee8380e158020f0999d0094814b4ae9379a0a
sadeeq <sadeejert@gmail.com>
2026-04-22 23:33:44 +0100

    refactor(models): add country_name field and modify data types

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     gender = models.CharField(max_length=10)
     gender_probability = models.FloatField()
@@ -15,10 +15,11 @@ class Person(models.Model):
     age = models.IntegerField()
     age_group = models.CharField(max_length=20)
 
-    country_id = models.CharField(max_length=3)
+    country_id = models.CharField(max_length=2)
+    country_name = models.CharField(max_length=100)
     country_probability = models.FloatField()
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} ({self.country_id})"
+        return f"{self.name} ({self.country_name})"
